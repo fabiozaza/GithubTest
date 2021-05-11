2da7cfec54d10bdbc152b45aef9cc7c40a092921
legend <legendbotha@gmail.com>
2021-05-11 14:20:30 +0200

    Added
    Added Main Menu with Host and Join Session
    
    Added  Pickup and Drop Blueprint to Fps
    
    (AdvancedSessions needs to be in plugins folder. New Empty C++ Class with public enabled)

---
+++ Source/GithubTest/GithubTest.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GithubTest.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, GithubTest, "GithubTest" );
